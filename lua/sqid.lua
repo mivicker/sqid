547ab9ceba4b2c897bbaf215ee8f6bebd0ec053f
mvickers <vickers.mike@gmail.com>
2022-08-21 08:52:31 -0400

    created module

--- lua/sqid.lua
+++ lua/sqid.lua
@@ -1,2 +1,26 @@
-print("Here is a plugin")
+local sqid = {}
 
+local function with_defaults(options)
+    return {
+        name = options.name or "Harvey"
+    }
+end
+
+function sqid.setup(options)
+    sqid.options = with_defaults(options)
+
+    vim.api.nvim_create_user_command("SqidTest", sqid.test, {})
+end
+
+function sqid.is_configured()
+    return sqid.options ~= nil
+end
+
+function sqid.test()
+    if not sqid.is_configured() then
+        return
+    end
+
+    local greeting = sqid.greeting(sqid.options.name)
+    print(greeting)
+end
