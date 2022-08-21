1491cc69be133eb793b27644f18fc1db4d929e5a
mvickers <vickers.mike@gmail.com>
2022-08-21 11:09:30 -0400

    this should work grabbing a line

--- lua/sqid/str_arm.lua
+++ lua/sqid/str_arm.lua
@@ -9,8 +9,7 @@ function str_arm.greet()
     local pos = vim.api.nvim_win_get_cursor(0)
     print(vim.inspect(pos))
     local line = vim.api.nvim_buf_get_lines(0, pos[1], pos[1] + 1, true)
-    print(vim.inspect(line))
-    return "Hello " .. "Harvey baby"
+    return "Hello " .. line[1]
 end
 
 return str_arm
