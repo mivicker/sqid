41fd7754abdb0da8a30e416de75a370eafc6c3d0
mvickers <vickers.mike@gmail.com>
2022-08-21 11:03:41 -0400

    more debugging

--- lua/sqid/str_arm.lua
+++ lua/sqid/str_arm.lua
@@ -6,9 +6,10 @@ local str_arm = {}
 -- vim.api.nvim_buf_get_lines(0, pos, pos + 1, true)
 
 function str_arm.greet()
-    local row, _ = vim.api.nvim_win_get_cursor(0)
-    local line = vim.api.nvim_buf_get_lines(0, row, row + 1, true)
-    return "Hello " .. line
+    local pos = vim.api.nvim_win_get_cursor(0)
+    print(vim.inspect(pos))
+    -- local line = vim.api.nvim_buf_get_lines(0, pos.row, row + 1, true)
+    return "Hello " .. "Harvey"
 end
 
 return str_arm
