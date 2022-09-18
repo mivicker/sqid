25448a7a74e3c3a4652232feb1f5e28228cb3558
mvickers <vickers.mike@gmail.com>
2022-09-18 19:25:36 -0400

    may have figured it out

--- lua/sqid/feeler_arm.lua
+++ lua/sqid/feeler_arm.lua
@@ -5,17 +5,22 @@ local feeler_arm = {}
 
 function feeler_arm.open_window()
     buf = api.nvim_create_buf(false, true)
+
     api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
-    local width = api.nvim_buf_get_option(0, 'columns')
-    local height = api.nvim_buf_get_option(0, 'lines')
+    -- get dimensions
+    local width = api.nvim_get_option('columns')
+    local height = api.nvim_get_option('lines')
 
+    -- calc floating window size
     local win_height = math.ceil(height * 0.8 - 4)
     local win_width = math.ceil(width * 0.8)
 
+    -- and its starting position
     local row = math.ceil((height - win_height) / 2 - 1)
     local col = math.ceil((width - win_width) / 2)
 
+    -- set some options
     local opts = {
         style = 'minimal',
         relative = 'editor',
